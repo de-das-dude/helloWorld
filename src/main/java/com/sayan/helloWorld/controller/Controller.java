3f8b76c5783604f3c80ca2ce9490d960731abcc3
dedas <dedas@DESKTOP-7L7531O>
2021-09-25 18:47:52 +0530

    setup as spring config client

--- src/main/java/com/sayan/helloWorld/controller/Controller.java
+++ src/main/java/com/sayan/helloWorld/controller/Controller.java
@@ -1,19 +1,19 @@
 package com.sayan.helloWorld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
 @RestController
 @RequestMapping("/mainController")
 public class Controller {
-	@GetMapping(value = "/{text}")
-	public String helloWorldApi(@PathVariable("text") String text)
-	{
-		return text;
+
+	@Value("${helloWorld.property.text:default}")
+	private String property;
+
+	@GetMapping(value = "/")
+	public String helloWorldApi() {
+		return property;
 	}
 }
