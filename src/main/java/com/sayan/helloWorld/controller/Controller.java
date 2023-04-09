8740bea45c6083365a20f5a24a48423a0b9d54d3
de-das-dude <de.das.dude@gmail.com>
2023-04-09 21:50:43 +0530

    zz

--- src/main/java/com/sayan/helloWorld/controller/Controller.java
+++ src/main/java/com/sayan/helloWorld/controller/Controller.java
@@ -1,11 +1,17 @@
 package com.sayan.helloWorld.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.sayan.helloWorld.dto.Dto;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -17,20 +23,46 @@ public class Controller {
 	@Value("${helloWorld.property.text:false}")
 	private boolean property;
 
+	@Value("${uri}")
+	String uri;
+
 	@GetMapping(value = "/doStuff")
 	public boolean doStuff() {
-		log.info("doing stuff");
+		
+		if(!property)
+		{
+			return property;
+		}
+		
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "http://localhost:8010/mainController/postHello";
+
+		System.out.println(uri);
+		restTemplate.postForLocation(uri, void.class);
 		
-		restTemplate.postForLocation(url, void.class);
+		restTemplate = new RestTemplate();
+		url  = "http://localhost:8010/mainController/getDto";
 		
+		ResponseEntity<Dto[]> response = restTemplate.getForEntity(url, Dto[].class);
+		Dto[] dtoList = response.getBody();
+		
+		log.info("**{}**{}**", dtoList[0].getMessage(), dtoList[1].getMessage());
+
 		return property;
 	}
-	
+
 	@PostMapping(value = "/postHello")
 	public void postHello() {
 		log.info("POSTED HELLO");
 	}
 	
+	@GetMapping(value = "/getDto")
+	public List<Dto> getDto(){
+		List<Dto> dtoList = new ArrayList<>();
+		dtoList.add(new Dto("hello"));
+		dtoList.add(new Dto("world"));
+		
+		return dtoList;
+	}
+
 }
---
+++ src/main/java/com/sayan/helloWorld/dto/Dto.java
@@ -0,0 +1,18 @@
+package com.sayan.helloWorld.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Dto {
+
+	@JsonProperty("message")
+	String message;
+}
